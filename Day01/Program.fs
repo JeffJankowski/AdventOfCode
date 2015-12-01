89e5b1eab23421ca7e941ee3d4c88ebbd5bf59d6
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2015-12-01 13:23:38 -0800

    Initial Commit

---
+++ Day01/Program.fs
@@ -0,0 +1,28 @@
+﻿// Jeff Jankowski - 12/01/2015
+// http://adventofcode.com/day/1
+
+let mapToInt = 
+    function 
+    | '(' -> 1
+    | ')' -> -1
+    | _ -> 0
+
+
+[<EntryPoint>]
+let main argv = 
+    let input = System.IO.File.ReadLines("..\..\input.txt")
+
+    input 
+    |> Seq.head 
+    |> Seq.map mapToInt 
+    |> Seq.sum
+    |> printfn "Ending Floor: %d" 
+
+    input 
+    |> Seq.head 
+    |> Seq.map mapToInt 
+    |> Seq.scan (fun floor curr -> floor + curr) 0
+    |> Seq.findIndex (fun floor -> floor < 0)
+    |> printfn "First Basement Position: %d" 
+
+    System.Console.Read()
---
+++ Day02/Program.fs
@@ -0,0 +1,7 @@
+﻿// Jeff Jankowski - 12/02/2015
+// http://adventofcode.com/day/2
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
