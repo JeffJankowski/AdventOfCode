21e3505e7e4ed883dad67f005e23b31943a8348d
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2015-12-14 13:52:17 -0800

    Changed from 4-tuple to record type

--- Day14/Program.fs
+++ Day14/Program.fs
@@ -3,39 +3,40 @@
 
 open System
 
-let step (fly, rest, dist) ((name, spd, flyt, restt) : (string * int * int * int)) = 
-    if fly < flyt then (fly+1, rest, dist + spd)
-    elif rest < restt then (fly, rest+1, dist)
-    else (1, 0, dist + spd)
+type Deer = { name: string; spd: int; flyT: int; restT: int }
+
+let step (fly, rest, dist) (deer: Deer) = 
+    if fly < deer.flyT then (fly+1, rest, dist + deer.spd)
+    elif rest < deer.restT then (fly, rest+1, dist)
+    else (1, 0, dist + deer.spd)
 
 let runDist deer = 
     [1..2503]
     |> List.fold (fun hist _ -> (step (hist |> List.head) deer :: hist) ) [(0,0,0)]
     |> List.map (fun (_,_,dist) -> dist)
 
-let runPts (deers : (string * int * int * int)[]) = 
-    let runs = deers |> Array.map (fun (name, spd, flyt, restt) -> 
-        (name, runDist (name, spd, flyt, restt) |> List.rev |> List.toArray))
+let runPts (deers : Deer[]) = 
+    let runs = deers |> Array.map (fun d -> (d.name, runDist d |> List.rev |> List.toArray))
     [1..2503]
     |> List.fold (fun (scores : Map<string,int>) i ->
         let order = runs |> Array.map (fun (n, h) -> (n, h.[i])) |> Array.sortBy snd |> Array.rev
         order 
         |> Seq.takeWhile (fun (name, dist) -> dist = (snd order.[0]))
         |> Seq.fold (fun sc (n,_) -> sc.Add (n, (sc.Item n)+1)) scores
-        ) (deers |> Array.map (fun (n,_,_,_) -> (n, 0)) |> Map.ofArray)
+        ) (deers |> Array.map (fun d -> (d.name, 0)) |> Map.ofArray)
             
 
 [<EntryPoint>]
 let main argv = 
     let map = 
-       IO.File.ReadAllLines("..\..\input.txt")
+        IO.File.ReadAllLines("..\..\input.txt")
         |> Array.map (fun s ->
             let split = s.Split(' ')
             let nums = 
                 split 
                 |> Array.filter (fun st -> st |> Seq.forall Char.IsDigit) 
                 |> Array.map Int32.Parse
-            (split.[0], nums.[0], nums.[1], nums.[2]) )
+            { name = split.[0]; spd = nums.[0]; flyT = nums.[1]; restT = nums.[2] } )
             
     map
     |> Array.map (fun d -> runDist d |> List.head)
