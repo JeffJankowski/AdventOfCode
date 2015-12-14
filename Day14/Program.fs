f452fe00d14f33eaa39329152ce4866eb7947ac8
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2015-12-13 23:44:05 -0800

    cleanup

--- Day14/Program.fs
+++ Day14/Program.fs
@@ -13,18 +13,15 @@ let runDist deer =
     |> List.fold (fun hist _ -> (step (hist |> List.head) deer :: hist) ) [(0,0,0)]
     |> List.map (fun (_,_,dist) -> dist)
 
-
 let runPts (deers : (string * int * int * int)[]) = 
     let runs = deers |> Array.map (fun (name, spd, flyt, restt) -> 
         (name, runDist (name, spd, flyt, restt) |> List.rev |> List.toArray))
-
     [1..2503]
     |> List.fold (fun (scores : Map<string,int>) i ->
-        let order = runs |> Array.map (fun (name, hist) -> (name, hist.[i])) |> Array.sortBy snd |> Array.rev
-        let leaders = order |> Seq.takeWhile (fun (name, dist) -> dist = (snd order.[0]))
-        leaders
+        let order = runs |> Array.map (fun (n, h) -> (n, h.[i])) |> Array.sortBy snd |> Array.rev
+        order 
+        |> Seq.takeWhile (fun (name, dist) -> dist = (snd order.[0]))
         |> Seq.fold (fun sc (n,_) -> sc.Add (n, (sc.Item n)+1)) scores
-        
         ) (deers |> Array.map (fun (n,_,_,_) -> (n, 0)) |> Map.ofArray)
             
 
@@ -34,7 +31,10 @@ let main argv =
        IO.File.ReadAllLines("..\..\input.txt")
         |> Array.map (fun s ->
             let split = s.Split(' ')
-            let nums = split |> Array.filter (fun st -> st |> Seq.forall Char.IsDigit) |> Array.map Int32.Parse
+            let nums = 
+                split 
+                |> Array.filter (fun st -> st |> Seq.forall Char.IsDigit) 
+                |> Array.map Int32.Parse
             (split.[0], nums.[0], nums.[1], nums.[2]) )
             
     map
