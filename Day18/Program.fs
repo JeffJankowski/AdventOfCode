b78fa1dc41923b8767cfc8cf233e17541c75663e
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2015-12-17 19:04:37 -0800

    cleanup

---
+++ Day18/Program.fs
@@ -0,0 +1,15 @@
+﻿// Jeff Jankowski - 12/18/2015
+// http://adventofcode.com/day/18
+
+open System
+
+
+[<EntryPoint>]
+let main argv = 
+    let input = IO.File.ReadLines "..\..\input.txt"
+
+    input 
+    |> ignore
+
+
+    Console.Read ()
