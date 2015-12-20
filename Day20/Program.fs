bef1534dd2af057ed726563c6d9805ff58a81bf0
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2015-12-19 22:31:02 -0800

    Day 20 Complete
    Elves delivering presents

--- Day20/Program.fs
+++ Day20/Program.fs
@@ -1,13 +1,25 @@
 ﻿// Jeff Jankowski - 12/20/2015
 // http://adventofcode.com/day/20
 
-open System
-
-
+let factors number = seq {
+    for divisor in 1 .. (float >> sqrt >> int) number do
+    if number % divisor = 0 then
+        yield (number, divisor)
+        yield (number, number / divisor) }
 
 [<EntryPoint>]
 let main argv = 
-    let input = IO.File.ReadAllLines "..\..\input.txt"
+    let input = 29000000
+    let find filt pres = 
+        Seq.initInfinite (fun i -> 
+            (factors i) 
+            |> Seq.distinctBy snd 
+            |> Seq.filter filt
+            |> Seq.sumBy snd 
+            |> (*) pres )
+        |> Seq.findIndex (fun sum -> sum >= input)
 
+    printfn "10 Presents:      %d" (find (fun _ -> true) 10)
+    printfn "11 and 50 Houses: %d" (find (fun (n,fact) -> n/fact <= 50) 11)
 
-    Console.Read () 
+    System.Console.Read () 
