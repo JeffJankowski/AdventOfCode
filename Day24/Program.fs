820784cf57ab6cacdb1c88b77d773cf51d47b1bf
Jeff Jankowski <jeffrey.jankowski@gmail.com>
2015-12-23 23:20:19 -0800

    Day 24 Completed
    Sleigh balancing act

---
+++ Day24/Program.fs
@@ -0,0 +1,24 @@
+﻿// Jeff Jankowski - 12/24/2015
+// http://adventofcode.com/day/24
+
+open System
+open Helpers
+
+let minQE nums grps =
+    let grpSum = (nums |> List.sum) / grps
+    [2..(nums.Length/grps - 1)]
+    |> List.map (fun n -> comb n nums)
+    |> List.concat
+    |> List.filter (fun cmb -> cmb |> List.sum = grpSum)
+    |> Seq.groupBy (fun cmb -> cmb.Length)
+    |> Seq.minBy (fun (len,_) -> len) |> snd
+    |> Seq.map (fun cmb -> cmb |> List.map int64 |> List.reduce (*))
+    |> Seq.min
+
+[<EntryPoint>]
+let main argv = 
+    let nums = IO.File.ReadAllLines "..\..\input.txt" |> Array.map Int32.Parse |> Array.toList
+    minQE nums 3 |> printfn "3 Groups: %d"
+    minQE nums 4 |> printfn "4 Groups: %d"
+
+    Console.Read ()
